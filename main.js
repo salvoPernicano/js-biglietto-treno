b215e24d199068584b55ffaf2302c6caf3bd2863
salvoPernicano <salvo.pernicano@gmail.com>
2023-12-22 18:28:14 +0100

    refactor codice js

--- main.js
+++ main.js
@@ -1,21 +1,19 @@
-window.addEventListener("DOMContentLoaded", (e)=>{
+function confirm() {
+  let kilometri = document.getElementById("kilometri").value;
+  let age = document.getElementById("age").value;
+  let tariffa = kilometri * 0.21;
 
-    let distanza = prompt("quanti km viaggerai?");
-    let age = prompt("quanti anni hai?")
-    const tariffa = 0.21;
-    let prezzo = distanza * tariffa;
-    if (age >= 65){
-        let senior = ((prezzo * 60) / 100).toFixed(2);
-        document.getElementById("price").innerHTML = `Il suo prezzo è: €${senior}`;
-    } else if (age < 65 && age >= 18){
-        document.getElementById("price").innerHTML = `Il suo prezzo è: €${prezzo.toFixed(2)}`;
-    } else {
-        let minorenne = ((prezzo * 80) / 100).toFixed(2);
-        document.getElementById("price").innerHTML = `Il suo prezzo è: €${minorenne}`;
-    }
-})
-
-
-
-
-
+  if (age >= 65) {
+    let senior = ((tariffa * 60) / 100).toFixed(2);
+    document.getElementById("price").innerHTML = `Il suo prezzo è: €${senior}`;
+  } else if (age < 65 && age >= 18) {
+    document.getElementById(
+      "price"
+    ).innerHTML = `Il suo prezzo è: €${tariffa.toFixed(2)}`;
+  } else {
+    let minorenne = ((tariffa * 80) / 100).toFixed(2);
+    document.getElementById(
+      "price"
+    ).innerHTML = `Il suo prezzo è: €${minorenne}`;
+  }
+}

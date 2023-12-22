519d1f5cd75678d486121d3fef834a168582d4a6
salvoPernicano <salvo.pernicano@gmail.com>
2023-12-22 14:46:09 +0100

    feat stile

--- main.js
+++ main.js
@@ -1,17 +1,20 @@
-let distanza = prompt("quanti km viaggerai?");
-let age = prompt("quanti anni hai?")
-const tariffa = 0.21;
-let prezzo = distanza * tariffa;
+window.addEventListener("DOMContentLoaded", (e)=>{
 
-if (age >= 65){
-    let senior = ((prezzo * 60) / 100).toFixed(2);
-    alert(`il suo prezzo e ${senior}`) 
-} else if (age < 65 && age >= 18){
-    alert(`il suo prezzo e ${prezzo.toFixed(2)}` )
-} else {
-    let minorenne = ((prezzo * 80) / 100).toFixed(2);
-    alert(`il suo prezzo e ${minorenne}`)
-}
+    let distanza = prompt("quanti km viaggerai?");
+    let age = prompt("quanti anni hai?")
+    const tariffa = 0.21;
+    let prezzo = distanza * tariffa;
+    if (age >= 65){
+        let senior = ((prezzo * 60) / 100).toFixed(2);
+        document.getElementById("price").innerHTML = `Il suo prezzo è: €${senior}`;
+    } else if (age < 65 && age >= 18){
+        document.getElementById("price").innerHTML = `Il suo prezzo è: €${prezzo.toFixed(2)}`;
+    } else {
+        let minorenne = ((prezzo * 80) / 100).toFixed(2);
+        document.getElementById("price").innerHTML = `Il suo prezzo è: €${minorenne}`;
+    }
+})
+
 
 
 
